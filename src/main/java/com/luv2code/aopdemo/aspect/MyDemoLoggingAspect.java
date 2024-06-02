b43012702f1d5a88767ac73a80aa8d3c33e5fb15
nkserveren26 <norinoritoshi@outlook.jp>
2024-06-02 22:27:52 +0900

    Pointcutアノテーションの学習。

--- src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,6 +2,7 @@ package com.luv2code.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -12,7 +13,10 @@ public class MyDemoLoggingAspect {
 
     // let's start with an @Before advice
 
-    @Before("execution(* com.luv2code.aopdemo.dao.*.*(..))")
+    @Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
+    private void forDaoPackage() {}
+
+    @Before("forDaoPackage()")
     public void beforeAddAccountAdvice() {
         System.out.println("\n=====>>> Executing @Before advice on method");
     }
