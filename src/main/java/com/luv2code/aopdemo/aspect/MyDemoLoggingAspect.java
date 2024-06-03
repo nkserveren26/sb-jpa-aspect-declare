53c100c32f14fbfd05c41ea08aaeb296aec02018
nkserveren26 <norinoritoshi@outlook.jp>
2024-06-03 20:50:33 +0900

    Pointcutアノテーションの学習。 複数のAOPで使いまわし

--- src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -8,16 +8,16 @@ import org.springframework.stereotype.Component;
 @Aspect
 @Component
 public class MyDemoLoggingAspect {
-
-    // this is where we add all of our related advices logging
-
-    // let's start with an @Before advice
-
     @Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
     private void forDaoPackage() {}
 
     @Before("forDaoPackage()")
     public void beforeAddAccountAdvice() {
         System.out.println("\n=====>>> Executing @Before advice on method");
+    }
+
+    @Before("forDaoPackage()")
+    public void performApiAnalytics() {
+        System.out.println("\n=====>>> Performing API analytics");
     }
 }
