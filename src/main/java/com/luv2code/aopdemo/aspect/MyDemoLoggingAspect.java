d6588b096825d94bcb8a8ce002f08541f5d5f417
nkserveren26 <norinoritoshi@outlook.jp>
2024-06-13 08:35:21 +0900

    Around Adviceの学習。

--- src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/main/java/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,6 +2,7 @@ package com.luv2code.aopdemo.aspect;
 
 import com.luv2code.aopdemo.Account;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
@@ -13,6 +14,22 @@ import java.util.List;
 @Component
 @Order(2)
 public class MyDemoLoggingAspect {
+
+    @Around("execution(* com.luv2code.aopdemo.dao.service.*.getFortune(..))")
+    public Object aroundGetFortune (
+            ProceedingJoinPoint theProceeding) throws Throwable {
+
+        // print out method we are advising on
+
+        // get begin timestamp
+
+        // now, let's execute the method
+
+        // get end timestamp
+
+        // compute duration and display it
+        return null;
+    }
 
     @After("execution(* com.luv2code.aopdemo.dao.AccountDAO.findAccounts(..))")
     public void afterFinallyFindAccountsAdvice(JoinPoint theJoinPoint) {
